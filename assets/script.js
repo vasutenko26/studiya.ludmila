91685c41dfb6af7f904e57f07674c7505691ce88
vasutenko26 <vess.marketing1@gmail.com>
2025-08-13 19:14:28 +0300

    Delete assets directory

--- assets/script.js
+++
@@ -1,3 +0,0 @@
-function openWA(){window.open('https://wa.me/380000000000?text=Вітаю! Хочу записатися в [Назва салону].','_blank')}
-function openTG(){window.open('https://t.me/your_salon','_blank')}
-function callNow(){window.location.href='tel:+380000000000'}
